f045f384ee625e48ce313c86fb0b91eaf59e54c0
Junaid Ali <gunaidali75@gmail.com>
2023-11-07 06:22:00 -0800

    Initial commit

---
+++ Cal1.ts
@@ -0,0 +1,51 @@
+import {sum} from "./sum.js";
+import {sub} from "./sub.js";
+import {mul} from "./mul.js";
+import {div} from "./div.js";
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+{
+    type: "number",
+    name: "num1",
+    message: "Enter First Number",
+},
+{
+    type: "number",
+    name: "num2",
+    message: "Enter Second Number",
+},
+{
+    type: "list",
+    name: "operator",
+    choices: ["+", "-","*","/"],
+    message: "select operator",
+},
+
+]);
+
+let {num1, num2, operator} = answer;
+if(num1 && num2 && operator){
+    let result : number;
+    if (operator ==="+"){
+        let result = sum(answer.num1,answer.num2);
+        console.log("result",result);
+    }else if (operator === "+"){
+        let result = sum (answer.num1, answer.num2);
+        console.log("Result",result);
+
+    }else if (operator === "-"){
+        let result = sub (answer.num1, answer.num2);
+        console.log("Result",result);
+    
+    }else if (operator === "*"){
+        let result = mul (answer.num1, answer.num2);
+        console.log("Result",result);
+    
+    }else if (operator === "/"){
+        let result = div (answer.num1, answer.num2);
+        console.log("Result",result);
+    }
+}
+
